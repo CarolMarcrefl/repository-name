3df548ad3220efd0e54811296cd63e678a75d8a8
vmuser <carolinemeta26@gmail.com>
2025-05-28 10:34:32 +0300

    create models

--- warehouse_management/manage.py
+++ warehouse_management/manage.py
@@ -2,6 +2,24 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class oaexam(models.Model):
+    exam_name = models.CharField(max_length=255, verbose_name="Название экзамена")
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания записи")
+    exam_date = models.DateTimeField(verbose_name="Дата проведения экзамена")
+    exam_image = models.ImageField(upload_to='exam_images/', verbose_name="Изображение задания")
+    users = models.ManyToManyField(User, verbose_name="Пользователи, которые должны писать экзамен")
+    is_public = models.BooleanField(default=False, verbose_name="Опубликовано")
+
+    def __str__(self):
+        return self.exam_name
+
+    class Meta:
+        verbose_name = "Экзамен"
+        verbose_name_plural = "Экзамены"
 
 
 def main():
