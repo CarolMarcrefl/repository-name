ed30a5af0f2e19f1baaa1a8a568029ae44ff1562
vmuser <carolinemeta26@gmail.com>
2025-05-28 11:46:04 +0300

    Настроена админка для oaexam: поиск, фильтры, M2M, превью изображений

--- warehouse_management/companies/admin.py
+++ warehouse_management/companies/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import oaexam
 
-# Register your models here.
+
+class ExamAdmin(admin.ModelAdmin):
+    # Поля, которые отображаются в списке
+    list_display = ('exam_name', 'created_at', 'exam_date', 'is_public')
+
+    # Поиск по названию экзамена и email пользователя
+    search_fields = ('exam_name', 'users__email')  # users__email - связь с User
+
+    # Фильтр по is_public и дате создания
+    list_filter = ('is_public', 'created_at')
+
+    # Иерархия по дате экзамена (появится под строкой поиска)
+    date_hierarchy = 'exam_date'
+
+    # Удобное редактирование M2M (users) - два списка
+    filter_horizontal = ('users',)
+
+    # Чтобы избежать проблем с загрузкой изображений в админке
+    readonly_fields = ('exam_image_preview',)
+
+    def exam_image_preview(self, obj):
+        if obj.exam_image:
+            from django.utils.html import format_html
+            return format_html('<img src="{}" width="150" />', obj.exam_image.url)
+        return "Нет изображения"
+
+    exam_image_preview.short_description = "Превью изображения"
+
+
+# Регистрация модели в админке
+admin.site.register(oaexam, ExamAdmin)
--- warehouse_management/manage.py
+++ warehouse_management/manage.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class oaexam(models.Model):
+class aoexam(models.Model):
     exam_name = models.CharField(max_length=255, verbose_name="Название экзамена")
     created_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания записи")
     exam_date = models.DateTimeField(verbose_name="Дата проведения экзамена")
